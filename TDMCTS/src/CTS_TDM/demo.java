17793bf95661058ab6253237bcbf037750c7eee0
“Alok <alok.knsharma@gmail.com>
2019-11-10 11:55:06 -0500

    hello

--- TDMCTS/src/CTS_TDM/demo.java
+++ TDMCTS/src/CTS_TDM/demo.java
@@ -5,7 +5,7 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello World");
-		System.out.println("Good Morning");
+		System.out.println("Good evening");
 
 	}
 
